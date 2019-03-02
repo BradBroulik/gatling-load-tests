911a9006b869fd1a032c8352bcd53ac5ba24d331
Broulik, Brad M <Brad.M.Broulik@HealthPartners.Com>
2019-03-02 13:35:38 -0600

    Update unauth test

--- user-files/simulations/api/api-itunes-unauthenticated-test.scala
+++ user-files/simulations/api/api-itunes-unauthenticated-test.scala
@@ -10,17 +10,13 @@ class ItunesApiUnauthenticatedLoadTest extends Simulation {
     private val loadTestName = "api-itunes-load-test"
     private val repeatTimes = 1 // How often to repeat the test scenario per user
     private val httpProtocol = http.baseUrl("https://itunes.apple.com")
-    private val users = csv("users.csv") 
     private val loadTestAPI = repeat(repeatTimes, "n") {
         exec(http(loadTestName)
             .get("/search?term=michael+jackson&limit=25")            
-            .basicAuth("${username}", "${password}")
         )
     }
 
-    private val scn = scenario("page")
-        .feed(users)
-        .exec(loadTestAPI)
+    private val scn = scenario("page").exec(loadTestAPI)
 
     // Run the load test with your preferred load (rampUsers & repeatTimes) within your preferred duration (seconds)
     setUp(scn.inject(rampUsers(10) during (20 seconds))).protocols(httpProtocol)
